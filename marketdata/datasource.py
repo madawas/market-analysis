0ff8344f26261ba7b7a8ff2a874f29d0345e3e5d
Madawa Soysa <madawa.rc@gmail.com>
2021-01-31 13:46:25 +1100

    Implement IEXCloud and AlphaVantage source connectors

--- marketdata/datasource.py
+++ marketdata/datasource.py
@@ -1,10 +1,24 @@
-import os
+# Copyright (c) 2021, Madawa Soysa
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import logging
+
 import requests
 from requests import HTTPError
 
-from util.exceptions import DataSourceException
 from util.constants import DataSourceConstants
+from util.exceptions import DataSourceException
 
 logger = logging.getLogger(__name__)
 
@@ -18,20 +32,29 @@ class DataSource(object):
     :raises DataSourceException if token is not provided for an authenticated data source
     :rtype DataSourceException
     """
-    def __init__(self, config):
+
+    def __init__(self, config: dict):
         try:
-            DataSource.__validate_config(config)
+            self._validate_config(config)
         except (TypeError, ValueError) as e:
             raise DataSourceException("Error occurred while validating the configuration to initialize the data source",
                                       e)
         else:
-            self.__name = config[DataSourceConstants.NAME]
-            self.__auth_token = config[DataSourceConstants.AUTH_TOKEN]
-            self.__base_url = config[DataSourceConstants.API_BASE_URL]
-            self.__config = config
+            self.__name = config.get(DataSourceConstants.NAME)
+            self._auth_token = config.get(DataSourceConstants.AUTH_TOKEN)
+            self._base_url = config.get(DataSourceConstants.API_BASE_URL)
+            self._config = config
 
-    def _prepare_url(self, resource):
-        return "{}{}".format(self.__base_url, resource)
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        self.__name = name
+
+    def _prepare_url(self, resource, **kwargs):
+        return "{}{}".format(self._base_url, resource)
 
     def get_data(self, resource, params=None, headers=None):
         """
@@ -43,26 +66,24 @@ class DataSource(object):
         :return: response data from the request
         :rtype dict
         """
-
-        if params is None:
-            params = {}
-        params["token"] = self.__auth_token
         url = self._prepare_url(resource)
         response = requests.get(url=url, params=params, headers=headers)
         logger.debug("REQUEST: {}".format(response.request.url))
         logger.debug("RESPONSE: {}".format(response.status_code))
         if response.status_code == requests.codes.ok:
+            logger.debug(response.text)
             return response.json()
         else:
             raise HTTPError("HTTP error occurred with status = {}, message = {}".format(response.status_code,
                                                                                         response.text))
 
-    @staticmethod
-    def __validate_config(config):
+    def _validate_config(self, config):
         """
-        Validating the configuration passed to initialize the data source
+        Validates the configuration passed to initialize the data source
 
         :param config: configuration dictionary
+        :raises ValueError when invalid value contains in a config parameter
+        :raises TypeError when value is present with an incorrect type
         """
         if not config or not isinstance(config, dict):
             raise TypeError("Configuration object is empty or not a required type")
@@ -71,20 +92,83 @@ class DataSource(object):
         if not config.get(DataSourceConstants.IS_LIBRARY) and not config.get(DataSourceConstants.API_BASE_URL):
             raise TypeError("Base Url is required when data source is not a library")
 
-        if not config.get(DataSourceConstants.TOKEN_ENV):
-            config[DataSourceConstants.TOKEN_ENV] = DataSourceConstants.DEFAULT_TOKEN_ENV
+        if not config.get(DataSourceConstants.TOKEN_ENV_VARIABLE):
+            config[DataSourceConstants.TOKEN_ENV_VARIABLE] = DataSourceConstants.DEFAULT_TOKEN_ENV
 
         # Set authentication token from environment variable if auth token is not included in the config
         if config.get(DataSourceConstants.IS_AUTHENTICATED) and not (
                 config.get(DataSourceConstants.AUTH_TOKEN) and
                 isinstance(config.get(DataSourceConstants.AUTH_TOKEN), str)):
-            config[DataSourceConstants.AUTH_TOKEN] = os.getenv(DataSourceConstants.TOKEN_ENV)
+            raise ValueError("API Authentication token is a required field and is missing in configuration")
 
-        if config[DataSourceConstants.AUTH_TOKEN] is None:
-            raise ValueError("API Authentication token is empty")
 
-        if not config.get(DataSourceConstants.RETRY_COUNT):
-            config[DataSourceConstants.RETRY_COUNT] = 3
+class IEXCloud(DataSource):
 
-        if not config.get(DataSourceConstants.RETRY_DELAY):
-            config[DataSourceConstants.RETRY_DELAY] = 0.5
+    __IEX_ENVIRONMENTS = ("sandbox", "production")
+
+    __IEX_VALID_VERSIONS = (
+        "stable",
+        "latest",
+        "v1"
+    )
+
+    def __init__(self, config: dict):
+        super().__init__(config)
+        self.__version = config.get(DataSourceConstants.API_VERSION)
+        self.__default_env = config[DataSourceConstants.API_ENVIRONMENT]
+
+    def _validate_config(self, config):
+        """
+        Validates the config related to IEX cloud data source
+
+        :param config: IEX Cloud configuration
+        :raises ValueError when invalid value contains in a config parameter
+        :raises TypeError when value is present with an incorrect type
+        """
+        if not config or not isinstance(config, dict):
+            raise TypeError("Configuration object is empty or not a required type")
+
+        if not config.get(DataSourceConstants.API_BASE_URL) and isinstance(config.get(
+                DataSourceConstants.API_BASE_URL), dict):
+            raise TypeError("IEX Cloud API base url is required and should be a dict")
+
+        if not config.get(DataSourceConstants.AUTH_TOKEN):
+            raise ValueError("Authentication token is required to connect with IEX Cloud API")
+
+        if not config.get(DataSourceConstants.API_ENVIRONMENT) or config.get(DataSourceConstants.API_ENVIRONMENT) not\
+                in IEXCloud.__IEX_ENVIRONMENTS:
+            logger.warning("Provided environment {} is invalid. Default environment {} will be used".format(
+                config.get(DataSourceConstants.API_ENVIRONMENT), IEXCloud.__IEX_ENVIRONMENTS[0]))
+            config[DataSourceConstants.API_ENVIRONMENT] = IEXCloud.__IEX_ENVIRONMENTS[0]
+
+        if not config.get(DataSourceConstants.API_VERSION):
+            config[DataSourceConstants.API_VERSION] = IEXCloud.__IEX_VALID_VERSIONS[0]
+
+    def _prepare_url(self, resource, **kwargs):
+        env = kwargs.get(DataSourceConstants.API_ENVIRONMENT, self.__default_env)
+        version = kwargs.get(DataSourceConstants.API_VERSION, self.__version)
+        return "{}/{}{}".format(self._base_url[env], version, resource)
+
+    def get_data(self, resource, params=None, headers=None):
+        if params is None:
+            params = {}
+        params["token"] = self._auth_token
+        return super().get_data(resource, params, headers)
+
+
+class AlphaVantage(DataSource):
+    def __init__(self, config):
+        super().__init__(config)
+
+    def _prepare_url(self, resource, **kwargs):
+        return super()._prepare_url(resource)
+
+    def get_data(self, resource, params=None, headers=None):
+        if params is None or not (params.get("function") and params.get("symbol")):
+            raise ValueError("Parameters: function and symbol is required")
+        params["apikey"] = self._auth_token
+        return super().get_data("", params, headers)
+
+
+class YahooFinance(DataSource):
+    pass
--- util/constants.py
+++ util/constants.py
@@ -1,3 +1,17 @@
+# Copyright (c) 2021, Madawa Soysa
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 class DataSourceConstants(object):
 
     NAME: str = "name"
@@ -10,12 +24,10 @@ class DataSourceConstants(object):
 
     API_VERSION: str = "version"
 
+    API_ENVIRONMENT: str = "environment"
+
     AUTH_TOKEN: str = "authToken"
 
-    TOKEN_ENV: str = "authTokenEnv"
-
-    RETRY_COUNT: str = "retryCount"
-
-    RETRY_DELAY: str = "retryDelay"
+    TOKEN_ENV_VARIABLE: str = "authTokenEnv"
 
     DEFAULT_TOKEN_ENV: str = "DATASOURCE_TOKEN"
