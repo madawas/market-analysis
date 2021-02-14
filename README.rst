fa2b4765f3aad5f456a19116b8154cc198ded3e3
Madawa Soysa <madawa.rc@gmail.com>
2021-02-14 11:29:03 +1100

    Add setup configurations

--- README.rst
+++ README.rst
@@ -1,2 +1,21 @@
-Market Analysis v0.0.1
-======================
+Python Library for Extracting Stock Market Data From Multiple Sources
+=====================================================================
+
+This python package allows extracting stock market data from multiple sources such as `IEXCloud <https://iexcloud
+.io/>`_, `Alpha Vantage <https://www.alphavantage.co/>`_ `Yahoo! Finance <https://finance.yahoo.com>`_ and other
+custom sources which can be defined in the configuration.
+
+Quick Start
+-----------
+
+Ticker Module
+^^^^^^^^^^^^^
+
+The ``Ticker`` module enables extracting market data using the data source of your choice.
+
+.. code-block:: python
+
+        from marketdata import Ticker
+
+        aapl = Ticker('AAPL', 'IEXCloud')
+        summary = aapl.get_summary()
--- setup.py
+++ setup.py
@@ -1,5 +1,19 @@
+# Copyright (c) 2021, Madawa Soysa
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 def read(f):
@@ -12,8 +26,20 @@ setup(
     author="Madawa Soysa",
     author_email="madawa.rc@gmail.com",
     license="Apache 2.0",
-    url="http://packages.python.org/an_example_pypi_project",
-    packages=["marketdata"],
+    url="https://github.com/madawas/market-analysis",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 1 - Planning",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Version Control :: Git"
+    ],
+    packages=find_packages(exclude=["tests", "tests.*"]),
     long_description=read("README.rst"),
+    install_rqquires=read("requirements.txt").splitlines(),
+    package_data={
+        '': ['LICENSE.txt'],
+        'conf': ['conf/config.yaml']
+    },
     include_package_data=True
 )
